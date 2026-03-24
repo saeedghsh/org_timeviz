ec8a57590bfdba446dbc8e560da529b19df2b4af
Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>
2026-03-24 17:26:14 +0100

    Add org mode init file to config
    Signed-off-by: Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>

--- org_timeviz/config.py
+++ org_timeviz/config.py
@@ -36,7 +36,7 @@ class OrgSourcesConfig(_BaseConfig):
     """Describe how to locate Org files for reporting."""
 
     mode: Literal["emacs", "explicit"] = Field(default="emacs")
-    emacs_init_paths: list[str] = Field(default_factory=lambda: ["~/.emacs", "~/.emacs.d/init.el"])
+    emacs_init_paths: list[str]
     emacs_agenda_var: str = Field(default="org-agenda-files")
     explicit_files: list[str] = Field(default_factory=list)
 
